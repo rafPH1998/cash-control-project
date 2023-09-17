d5a2992cc292c114817106a318fadb47d4c3596f
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-09-17 12:11:35 -0300

    att

--- resources/js/pages/index.vue
+++ resources/js/pages/index.vue
@@ -148,17 +148,18 @@
                                         </div>
                                     </div>
 
-                                    <div class="flex flex-row-reverse">
+                                    <div class="flex flex-row-reverse" v-if="totalPrice > 0">
                                         <button
-                                            :disabled="statusBox || box"
+                                            :disabled="statusBox || box && currentMonthName === search"
                                             @click="closeBox()"
                                             class="mt-8 text-white font-bold rounded-full text-xs px-2"
-                                            :class="{ 'bg-green-500 hover:bg-green-600': !statusBox && !box, 'bg-gray-400 hover:bg-gray-500': statusBox || box }">
+                                            :class="{ 'bg-green-500 hover:bg-green-600': !statusBox && !box || currentMonthName !== search, 'bg-gray-400 hover:bg-gray-500': statusBox || box }">
                                             <span class="flex items-center">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-1">
+                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-1">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                 </svg>
-                                                <p v-if="statusBox || box" class="ml-1">Caixa Fechado</p>
+                                                <p v-if="statusBox || box && currentMonthName === search" class="ml-1">Caixa Fechado</p>
                                                 <p v-else class="ml-1">Fechar Caixa</p>
                                             </span>
                                         </button>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Gerenciamento de membros do futebol</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-[#fef9c3]">
